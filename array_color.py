0f72a55a9abe456c2a1743c838d461707b7d85f2
usausa <143808163+uasuas@users.noreply.github.com>
2024-01-03 23:21:17 +0900

    [update] 色の配列

--- array_color.py
+++ array_color.py
@@ -1,12 +1,27 @@
 import tkinter
 root = tkinter.Tk()
 root.title("配列で色を定義")
-cvs = tkinter.Canvas(width = 840, height = 160)
+cvs = tkinter.Canvas(width = 800, height = 600, bg = "black")
+# 配列で色を定義
+# rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+# iを０〜６まで１ずつ増える
+# for i in range(7):
+	# X座標を計算して、Xに代入
+	# X = i * 120
+	# 四角を生成して色を入れていく
+	# cvs.create_rectangle(X, 0, X+120, 160, fill=rainbow[i], width=0)
 
-rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-for i in range(7):
-	X = i * 120
-	cvs.create_rectangle(X, 0, X+120, 160, fill=rainbow[i], width=0)
+color = [
+  ["brown", "red", "orange", "gold"],
+  ["darkgreen", "green", "limegreen", "lime"],
+  ["navy", "blue", "skyblue", "cyan"]
+]
+
+for y in range(3):
+    for x in range(4):
+        X = x * 200
+        Y = y * 200
+        cvs.create_oval(X, Y, X+200, Y+200, fill=color[y][x])
 
 cvs.pack()
 root.mainloop()
