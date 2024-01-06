181bd6fcb516addedeb2b172106dbcb6212b0a0c
usausa <143808163+uasuas@users.noreply.github.com>
2024-01-06 19:52:55 +0900

    [update] コメント修正

--- mouse_circle.py
+++ mouse_circle.py
@@ -7,24 +7,26 @@ def move(e):
   global mx, my
   mx = e.x
   my = e.y
-
+# 下記の関数で使用するための、円の初期位置と半径を指定
 cx = 400
 cy = 300
 cr = 50
 # 移動条件の関数を定義
 def main():
   global cx, cy
-  # カーソルの位置から見て、円が下ならcyから5引く
+  # カーソルの位置から見て、円が上ならcyから1引く（円のY座標を1ピクセル上に移動）
   if cy>my:
     cy -= 1
+  # カーソルの位置から見て、円が下ならcyから1足す（円のY座標を1ピクセル下に移動）
   else:
     cy += 1
-  # カーソルの位置から見て、円が下ならcxから5引く
+  # カーソルの位置から見て、円が左ならcxから1引く（円のX座標を1ピクセル上に移動）
   if cx>mx:
     cx -= 1
+  # カーソルの位置から見て、円が右ならcxから1足す（円のX座標を1ピクセル下に移動）
   else:
     cx += 1
-  # 移動まえの円を削除
+  # 移動前の円を削除
   cvs.delete("all")
   # 削除後新たに上記のifの結果を反映した円を生成
   cvs.create_oval(cx-cr, cy-cr, cx+cr, cy+cr, fill="blue", outline="cyan")
@@ -33,6 +35,7 @@ def main():
 
 root = tkinter.Tk()
 root.title("ポインタを追いかける円")
+# 引数にFalseを指定することで、ウィンドウの幅と高さの両方向でのサイズ変更を無効化
 root.resizable(False, False)
 root.bind("<Motion>", move)
 cvs = tkinter.Canvas(width=800, height=600, bg="black")
