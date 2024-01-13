84df31825dbdd9b62a5cb19b60927089097f7d8f
usausa <143808163+uasuas@users.noreply.github.com>
2024-01-13 17:17:34 +0900

    [update] 解説記入

--- pressed_key_color.py
+++ pressed_key_color.py
@@ -2,12 +2,18 @@ import tkinter
 
 FNT = ("Times New Roman", 60)
 COLOR = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+# キーを押された時に呼び出す関数設定
 def pkey(e):
+  # kに押されたキーのシンボルを代入
   k = e.keysym
+  # 押されたキーが1~7にある時に対応する処理を設定
   if "1"<=k and k<="7":
+    # cに押されたキーを整数にして-1して代入（配列が0スタートのための処理）
     c = int(k)-1
     cvs.delete("all")
+    # 背景を押されたキーに応じたCOLORにする
     cvs["bg"] = COLOR[c]
+    # テキスト内容をCOLORに定義した文字列にし色を白に設定
     cvs.create_text(300, 150, text=COLOR[c], fill="white", font=FNT)
 
 root = tkinter.Tk()
