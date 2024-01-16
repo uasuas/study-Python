6b39dfba6d264e634c827d9a6b180ddcaae84bd7
usausa <143808163+uasuas@users.noreply.github.com>
2024-01-16 21:31:21 +0900

    [update] 002

--- AtCoder/002.py
+++ AtCoder/002.py
@@ -1,3 +1,5 @@
+# int 関数は与えられた引数を整数型に変換、input 関数はユーザーに対してプログラムへの入力を受け付ける
+# input().split()とすることで、文字列を空白で分割しリスト化、map関数を使うことで文字列から整数に変換
 A1, A2, A3 = map(int, input().split())
 total = A1 + A2 + A3
 print(total)
