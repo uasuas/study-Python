4252c88d66b99e5f35d28d199102603a673f81f1
usausa <143808163+uasuas@users.noreply.github.com>
2024-01-18 16:06:01 +0900

    [update] test1

--- AtCoder/test.py
+++ AtCoder/test.py
@@ -1,12 +1,26 @@
+# def fib_l(n):
+#     a, b = 1, 2
+#     if n == 1:
+#         return 1
+#     elif n == 2:
+#         return 2
+#     else:
+#         for _ in range(n - 2):
+#             a, b = b, a + b - 1
+#         return b
+
+# ans = fib_l(42)
+# print(ans)
+
 def fib_l(n):
+    a, b = 0, 1
     if n == 1:
-        return 1
+        return a
     elif n == 2:
-        return 2
+        return b
     else:
-        a, b = 1, 2
-        for _ in range(n - 2):
-            a, b = b, a + b - 1
+        for _ in range(n-2):
+            a, b = b, a + b
         return b
 
 ans = fib_l(42)
