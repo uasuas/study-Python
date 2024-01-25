45e6290aa588425cb8a0d41d76d9fbaf64c3fb08
usausa <143808163+uasuas@users.noreply.github.com>
2024-01-25 23:23:30 +0900

    [update] モグラ叩き

--- Game/moguratataki.py
+++ Game/moguratataki.py
@@ -3,22 +3,37 @@ import random
 
 FNT = ("System", 40)
 holes = [0, 0, 0, 0, 0]
+key = ""
+
+def pkey(e):
+  global key
+  key = e.keysym
+
 
 def main():
+  global key
   cvs.delete("all")
   for i in range(5):
     x = 200*i+100
     cvs.create_image(x, 160, image=img[holes[i]])
     cvs.create_text(x, 280, text=i+1, font=FNT, fill="yellow")
+    if holes[i]==2:
+      holes[i] = 0
 
   r = random.randint(0, 4)
-  if holes[r] == 0:
-    holes[r] = 1
-  else:
-    holes[r] = 0
+  holes[r] = 1
+  if "1"<=key and key<="5":
+    m = int(key)-1
+    x = m*200+100
+    cvs.create_image(x, 60, image=ham)
+    if holes[m]==1:
+      holes[m] = 2
+
+  key = ""
   root.after(330, main)
   
 root = tkinter.Tk()
+root.bind("<Key>", pkey)
 cvs = tkinter.Canvas(width=1000, height=320)
 cvs.pack()
 img = [
