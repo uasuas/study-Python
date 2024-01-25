740d55041ca3e918622db227dd26b28e66bada6c
usausa <143808163+uasuas@users.noreply.github.com>
2024-01-25 21:25:18 +0900

    [update] モグラランダム表示

--- Game/moguratataki.py
+++ Game/moguratataki.py
@@ -1,10 +1,22 @@
 import tkinter
+import random
+
+FNT = ("System", 40)
+holes = [0, 0, 0, 0, 0]
 
 def main():
+  cvs.delete("all")
   for i in range(5):
     x = 200*i+100
-    cvs.create_image(x, 160, image=img[0])
-  cvs.create_image(x, 60, image=ham)
+    cvs.create_image(x, 160, image=img[holes[i]])
+    cvs.create_text(x, 280, text=i+1, font=FNT, fill="yellow")
+
+  r = random.randint(0, 4)
+  if holes[r] == 0:
+    holes[r] = 1
+  else:
+    holes[r] = 0
+  root.after(330, main)
   
 root = tkinter.Tk()
 cvs = tkinter.Canvas(width=1000, height=320)
